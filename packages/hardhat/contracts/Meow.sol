ed0051884ebe1e42fc51e74de20cda2c909c0f0f
Mr Web3 <87449884+ysongit@users.noreply.github.com>
2024-01-29 18:08:09 -0500

    add contracts

---
+++ packages/hardhat/contracts/Meow.sol
@@ -0,0 +1,47 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "./XPToken.sol";
+import "./CatNFT.sol";
+import "./AccessoriesNFT.sol";
+
+contract Meow {
+
+  XPToken public xptoken;
+  CatNFT public catNFT;
+  AccessoriesNFT public accessoriesNFT;
+
+  mapping(address => uint) public ownerToPetID;
+  mapping(address => uint) public ownerToAccessoryID;
+
+  constructor(address token_address, address cats_address, address accessories_address) {
+    xptoken = XPToken(token_address);
+    catNFT = CatNFT(cats_address);
+    accessoriesNFT = AccessoriesNFT(accessories_address);
+  }
+
+  /**
+    * Create and mint Cat NFT
+    */
+  function createCat(string memory token_URI) external {
+    uint nft_id = catNFT.mint(msg.sender, token_URI);
+    ownerToPetID[msg.sender] = nft_id;
+  }
+
+  /**
+    * Earn XP
+    */
+  function earnXP() external {
+   xptoken.mint(msg.sender, 1000000000000000000);
+  }
+
+   /**
+    * Purchase accessory and mint it as NFT
+    */
+  function purchaseAccessory(string memory token_URI) external {
+    uint nft_id = accessoriesNFT.mint(msg.sender, token_URI);
+    ownerToAccessoryID[msg.sender] = nft_id;
+    xptoken.burn(msg.sender, 1000000000000000000);
+  }
+
+}
