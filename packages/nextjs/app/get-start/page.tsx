f5c3ea3a027ec8b7815a4702a8d93c2bb858d218
Mr Web3 <87449884+ysongit@users.noreply.github.com>
2024-01-31 20:51:09 -0500

    add pages

---
+++ packages/nextjs/app/get-start/page.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import Image from "next/image";
+import Link from "next/link";
+import type { NextPage } from "next";
+import { useRouter } from "next/navigation";
+import { useScaffoldContractRead } from "~~/hooks/scaffold-eth";
+import { useAccount } from "wagmi";
+
+const GetStart: NextPage = () => {
+  const { address } = useAccount();
+
+  const backgroundImageStyle = {
+    backgroundImage: 'url("/assets/cover1.jpg")',
+    backgroundSize: "cover",
+    backgroundRepeat: "no-repeat",
+    height: "100vh", // Set to full height of the viewport
+  };
+
+  const router = useRouter();
+  const poligonIdAuthentication = async () => {
+    router.push("/selectCat");
+  };
+  return (
+    <div className="">
+      <div style={backgroundImageStyle} onClick={poligonIdAuthentication}></div>
+    </div>
+  );
+};
+
+export default GetStart;
